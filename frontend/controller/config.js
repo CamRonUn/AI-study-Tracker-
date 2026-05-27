0e340a43a3fe037b90c0d90866e865b1003748cd
Cameron Taylor <camronun@gmail.com>
2026-05-27 14:07:12 +1000

    update base url

--- frontend/controller/config.js
+++ frontend/controller/config.js
@@ -1,7 +1,7 @@
 import * as SecureStore from 'expo-secure-store';
 import { Platform } from 'react-native'; // Import Platform to detect Web vs Mobile
 
-export const baseURL = 'http://127.0.0.1:8000'
+export const baseURL = 'https://ai-study-tracker-wrip.onrender.com'
 export async function loadToken() { 
     if (Platform.OS === 'web') {
         return localStorage.getItem('userToken');
